ae0b8d0d12ea4b9f829610521a7f259f176b4ca8
diebwesttt-dotcom <diebwes.ttt@gmail.com>
2026-08-01 17:53:57 +0200

    Update community-config.js

--- public/community-config.js
+++ public/community-config.js
@@ -7,7 +7,7 @@ window.COMMUNITY_CONFIG = {
       shortTitle: "Conan Exiles",
       game: "Conan Exiles Enhanced",
       description: "Eine brutale offene Welt voller Ruinen, Thralls und Bauprojekten, die garantiert viel kleiner geplant waren.",
-      image: "/assets/conan.jpg",
+      image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/440900/header.jpg",
       status: "checking",
       players: 0,
       maxPlayers: 20,
